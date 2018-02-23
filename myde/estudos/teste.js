b6e26ce205e38b2c24763d0293c1ca71a27240b4
CaldasBR <fcaldas@gmail.com>
2018-02-23 19:54:57 -0300

    <br>

--- myde/estudos/teste.js
+++ myde/estudos/teste.js
@@ -2,15 +2,15 @@ var teste1 = 'oi';
 function teste2() {
     var teste3 = 'tchau';
 
-     document.body.innerHTML = 'Dentro da função -  teste1:' + teste1;
-     document.body.innerHTML = 'Dentro da função -  teste2:' + teste2;
-     document.body.innerHTML = 'Dentro da função -  teste3:' + teste3;
+     document.body.innerHTML = 'Dentro da função -  teste1:' + teste1 + '<br>';
+     document.body.innerHTML = 'Dentro da função -  teste2:' + teste2 + '<br>';
+     document.body.innerHTML = 'Dentro da função -  teste3:' + teste3 + '<br>';
 }
 
 
- document.body.innerHTML = 'Fora da função -  teste1:' + teste1;
- document.body.innerHTML = 'Fora da função -  teste2:' + teste2;
- document.body.innerHTML = 'Fora da função -  teste3:' + teste3;
+ document.body.innerHTML = 'Fora da função -  teste1:' + teste1 + '<br>';
+ document.body.innerHTML = 'Fora da função -  teste2:' + teste2 + '<br>';
+ document.body.innerHTML = 'Fora da função -  teste3:' + teste3 + '<br>';
 
- document.body.innerHTML = 'Vou executar -  teste2:';
+ document.body.innerHTML = 'Vou executar -  teste2: <br>';
 teste2();
