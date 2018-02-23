dc32eedf25152b2cf368f9ff34a53b5223545bdb
CaldasBR <fcaldas@gmail.com>
2018-02-23 19:11:29 -0300

    estuda escopo

---
+++ myde/estudos/teste.js
@@ -0,0 +1,13 @@
+var teste1 = 'oi'
+var teste2 = function(){
+    var teste3 = 'tchau'
+
+    console.log('Dentro da função -  teste1:' + teste1);
+    console.log('Dentro da função -  teste2:' + teste2);
+    console.log('Dentro da função -  teste3:' + teste3);
+}
+
+
+console.log('Fora da função -  teste1:' + teste1);
+console.log('Fora da função -  teste2:' + teste2);
+console.log('Fora da função -  teste2:' + teste3);
