a7f2013d92023fb47ffc9a83a21e61ce1196331f
CaldasBR <fcaldas@gmail.com>
2018-02-23 19:53:32 -0300

    document.body.innerHTML

--- myde/estudos/teste.js
+++ myde/estudos/teste.js
@@ -2,15 +2,15 @@ var teste1 = 'oi';
 function teste2() {
     var teste3 = 'tchau';
 
-    console.log('Dentro da função -  teste1:' + teste1);
-    console.log('Dentro da função -  teste2:' + teste2);
-    console.log('Dentro da função -  teste3:' + teste3);
+     document.body.innerHTML = 'Dentro da função -  teste1:' + teste1;
+     document.body.innerHTML = 'Dentro da função -  teste2:' + teste2;
+     document.body.innerHTML = 'Dentro da função -  teste3:' + teste3;
 }
 
 
-console.log('Fora da função -  teste1:' + teste1);
-console.log('Fora da função -  teste2:' + teste2);
-console.log('Fora da função -  teste3:' + teste3);
+ document.body.innerHTML = 'Fora da função -  teste1:' + teste1;
+ document.body.innerHTML = 'Fora da função -  teste2:' + teste2;
+ document.body.innerHTML = 'Fora da função -  teste3:' + teste3;
 
-console.log('Vou executar -  teste2:');
+ document.body.innerHTML = 'Vou executar -  teste2:';
 teste2();
